fababfd34eed668056306bf55f6b15cadf9bba8a
josephclander <josephclander@gmail.com>
2024-04-15 12:54:38 +0100

    Add submit function

--- src/components/CreatePostForm.jsx
+++ src/components/CreatePostForm.jsx
@@ -1,7 +1,18 @@
+import { useRef } from "react";
+
 const CreatePostForm = () => {
+  const formRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formRef.current.value);
+    formRef.current.value = "";
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <textarea
+        ref={formRef}
         name="text"
         id="text"
         cols="30"
